75d5303e3b83edbdfb2c7f987a8dff0cf80657f8
Izan-Abramovici-Cabrera <Izanito2005@gmail.com>
2023-11-28 12:51:13 +0100

    cambios

--- php/Inicio-php.php
+++ php/Inicio-php.php
@@ -1,3 +1,29 @@
 <?php
-echo "Hola mundo";
-echo "hola";
+$num1 = 00;
+$num2 = 1;
+$num3 = 260;
+
+if($num1 > $num2 && $num1 > $num3) {
+    if($num2>$num3){
+        echo "$num1 > $num2 > $num3";
+        }   else
+            echo "$num1 > $num3 > $num2";
+    }else{
+        if($num1 < $num2 && $num1 < $num3) {
+            if($num2>$num3) {
+                echo "$num2 > $num3 > $num1";
+                }      else  {
+                echo "$num3 > $num2 > $num1";
+            } 
+        }else {
+                if( $num2 > $num3){
+            echo "$num2 > $num1 > $num3";
+            } else { 
+                echo "$num3 > $num1 > $num2";
+            }
+        }
+    } 
+      
+
+
+
